6f5642c5b4096784830840f66aaf2f1142581fd1
Raphninefo <rsirvent@mail.sfsu.edu>
2024-08-04 12:56:20 -0700

    Update to black background with white text and list tech certifications

--- pages/index.js
+++ pages/index.js
@@ -15,32 +15,6 @@ export default function Home() {
           Hi, I'm <a href="https://www.linkedin.com/in/raphael-sirvent/" target="_blank" rel="noopener noreferrer">Raphaël</a>
         </h1>
 
-        <p className={styles.description}>
-          Please hire me because:
-        </p>
-
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h3>Same Team</h3>
-            <p>I excel in cross-functional collaboration, driving key projects to successful outcomes.</p>
-          </div>
-
-          <div className={styles.card}>
-            <h3>Dig Deep</h3>
-            <p>I thoroughly investigate and solve critical issues, ensuring long-term solutions and customer satisfaction.</p>
-          </div>
-
-          <div className={styles.card}>
-            <h3>KYC</h3>
-            <p>I prioritize understanding and addressing customer needs, ensuring they achieve their goals with our solutions.</p>
-          </div>
-
-          <div className={styles.card}>
-            <h3>Iterate To Greatness</h3>
-            <p>I continually improve solutions through rapid iteration and collaboration, delivering outstanding results.</p>
-          </div>
-        </div>
-
         <div className={styles.certifications}>
           <h2>My Tech Certifications</h2>
           <ul>
