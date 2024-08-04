b68cbcfd3fd2d058f7a0dc44e1223402eb092109
Raphninefo <rsirvent@mail.sfsu.edu>
2024-08-04 14:45:46 -0700

    Add new pages and navigation links, update title

--- pages/who-is-raph.js
+++ pages/who-is-raph.js
@@ -15,3 +15,20 @@ export default function WhyLeavingDO() {
     </div>
   );
 }
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function WhoIsRaph() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Who is Raph?</title>
+      </Head>
+      <main className={styles.main}>
+        <h1>Who is Raph?</h1>
+        <p>Content about who Raph is...</p>
+      </main>
+    </div>
+  );
+}
+
