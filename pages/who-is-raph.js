5db36855f6f76422690bcbd78d529017c4e73cce
Raphninefo <rsirvent@mail.sfsu.edu>
2024-08-04 14:30:00 -0700

    Add new pages and navigation links, update title

---
+++ pages/who-is-raph.js
@@ -0,0 +1,17 @@
+// Create the file pages/why-leaving-do.js with the following content
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function WhyLeavingDO() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Why would I consider leaving DO?</title>
+      </Head>
+      <main className={styles.main}>
+        <h1>Why would I consider leaving DO?</h1>
+        <p>Content about why Raph might consider leaving DO...</p>
+      </main>
+    </div>
+  );
+}
