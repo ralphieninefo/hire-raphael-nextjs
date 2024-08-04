6c5c68f11668616f2829c1c2a6741476b19769fb
Raphninefo <rsirvent@mail.sfsu.edu>
2024-08-04 14:47:35 -0700

    Fix multiple imports and exports in who-is-raph and why-leaving-do

--- pages/who-is-raph.js
+++ pages/who-is-raph.js
@@ -1,20 +1,3 @@
-// Create the file pages/why-leaving-do.js with the following content
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-
-export default function WhyLeavingDO() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Why would I consider leaving DO?</title>
-      </Head>
-      <main className={styles.main}>
-        <h1>Why would I consider leaving DO?</h1>
-        <p>Content about why Raph might consider leaving DO...</p>
-      </main>
-    </div>
-  );
-}
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
@@ -31,4 +14,3 @@ export default function WhoIsRaph() {
     </div>
   );
 }
-
