847819d3e2b7b39df51c6fffd75757d1ddd7cd8f
Raphninefo <rsirvent@mail.sfsu.edu>
2024-08-05 13:07:45 -0700

    Clean up About Me v2

--- pages/about-me.js
+++ pages/about-me.js
@@ -12,7 +12,7 @@ export default function AboutMe() {
         <p>Most people call me Raph.</p>
         <p>French is my first language.</p>
         <p>I love soccer (football) and my favorite player of all time is Zidane.</p>
-        <p>I’ve lived in Denton, Texas (shoutout Norah Jones) and Sacramento, but I’ve called San Francisco my home for the last 10 years.</p>
+        <p>I’ve lived in Denton, Texas (shoutout Norah Jones) and Sacramento (light the beam!), but I’ve called San Francisco my home for the last 10 years.</p>
         <p>I’m a people person, a gritty problem solver, and most importantly, a life-long learner.</p>
       </main>
     </div>
