019131f1d4af8e9ce957c8d48ee11d9f6c49ee8a
Raphninefo <rsirvent@mail.sfsu.edu>
2024-08-04 17:54:51 -0700

    Rename Why leave DO page to Why join Vercel and update links

---
+++ pages/why-join-vercel.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function WhyJoinVercel() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Why join Vercel?</title>
+      </Head>
+      <main className={styles.main}>
+        <h1>Why join Vercel?</h1>
+        <p>If I’m given the opportunity to work at Vercel, leaving DO would be a difficult decision.</p>
+        <p>I really like my customers.</p>
+        <p>I’ve learned a lot about back-end cloud (coming from FinTech had to learn).</p>
+        <p>DO and Vercel are also similar in many ways…</p>
+        <p>They have a Developer Ethos</p>
+        <p>Open Source Community</p>
+        <p>Simplifying or “Democratizing” access to Dev Tools</p>
+      </main>
+    </div>
+  );
+}
